ec6af9e43a086ad90a9356eb0592777f5c33c4ae
Angelo Guiao <angeloguiao.gci@gmail.com>
2025-03-08 21:51:19 +0800

    react

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-console.log(React);
+
 import { 
     Menubar,
     MenubarContent,
@@ -13,88 +13,90 @@ import { ModeToggle } from "./mode-toggle";
 
 const Navbar = () => {
     return (
-      <div className="overflow-hidden bg-white border-b fixed top-0 w-full z-10 flex items-center p-2 dark:bg-black">
-        
-        {/* Left-aligned Menubar */}
-        <div className="flex">
-            <Menubar className="border-0 shadow-none dark:bg-none">
-                <MenubarMenu>
-                    <MenubarTrigger>Music</MenubarTrigger>
-                    <MenubarContent>
-                        <MenubarItem>New Tab <MenubarShortcut>⌘T</MenubarShortcut></MenubarItem>
-                        <MenubarItem>New Window</MenubarItem>
-                        <MenubarSeparator />
-                        <MenubarItem>Share</MenubarItem>
-                        <MenubarSeparator />
-                        <MenubarItem>Print</MenubarItem>
-                    </MenubarContent>
-                </MenubarMenu>
-            </Menubar>
+        <React.Fragment>
+        <div className="overflow-hidden bg-white border-b fixed top-0 w-full z-10 flex items-center p-2 dark:bg-black">
+            
+            {/* Left-aligned Menubar */}
+            <div className="flex">
+                <Menubar className="border-0 shadow-none dark:bg-none">
+                    <MenubarMenu>
+                        <MenubarTrigger>Music</MenubarTrigger>
+                        <MenubarContent>
+                            <MenubarItem>New Tab <MenubarShortcut>⌘T</MenubarShortcut></MenubarItem>
+                            <MenubarItem>New Window</MenubarItem>
+                            <MenubarSeparator />
+                            <MenubarItem>Share</MenubarItem>
+                            <MenubarSeparator />
+                            <MenubarItem>Print</MenubarItem>
+                        </MenubarContent>
+                    </MenubarMenu>
+                </Menubar>
 
-            <Menubar className="border-0">
-                <MenubarMenu>
-                    <MenubarTrigger>File</MenubarTrigger>
-                    <MenubarContent>
-                        <MenubarItem>New Tab <MenubarShortcut>⌘T</MenubarShortcut></MenubarItem>
-                        <MenubarItem>New Window</MenubarItem>
-                        <MenubarSeparator />
-                        <MenubarItem>Share</MenubarItem>
-                        <MenubarSeparator />
-                        <MenubarItem>Print</MenubarItem>
-                    </MenubarContent>
-                </MenubarMenu>
-            </Menubar>
+                <Menubar className="border-0">
+                    <MenubarMenu>
+                        <MenubarTrigger>File</MenubarTrigger>
+                        <MenubarContent>
+                            <MenubarItem>New Tab <MenubarShortcut>⌘T</MenubarShortcut></MenubarItem>
+                            <MenubarItem>New Window</MenubarItem>
+                            <MenubarSeparator />
+                            <MenubarItem>Share</MenubarItem>
+                            <MenubarSeparator />
+                            <MenubarItem>Print</MenubarItem>
+                        </MenubarContent>
+                    </MenubarMenu>
+                </Menubar>
 
-            <Menubar className="border-0 shadow-none">
-                <MenubarMenu>
-                    <MenubarTrigger>Edit</MenubarTrigger>
-                    <MenubarContent>
-                        <MenubarItem>New Tab <MenubarShortcut>⌘T</MenubarShortcut></MenubarItem>
-                        <MenubarItem>New Window</MenubarItem>
-                        <MenubarSeparator />
-                        <MenubarItem>Share</MenubarItem>
-                        <MenubarSeparator />
-                        <MenubarItem>Print</MenubarItem>
-                    </MenubarContent>
-                </MenubarMenu>
-            </Menubar>
+                <Menubar className="border-0 shadow-none">
+                    <MenubarMenu>
+                        <MenubarTrigger>Edit</MenubarTrigger>
+                        <MenubarContent>
+                            <MenubarItem>New Tab <MenubarShortcut>⌘T</MenubarShortcut></MenubarItem>
+                            <MenubarItem>New Window</MenubarItem>
+                            <MenubarSeparator />
+                            <MenubarItem>Share</MenubarItem>
+                            <MenubarSeparator />
+                            <MenubarItem>Print</MenubarItem>
+                        </MenubarContent>
+                    </MenubarMenu>
+                </Menubar>
 
-            <Menubar className="border-0 shadow-none">
-                <MenubarMenu>
-                    <MenubarTrigger>View</MenubarTrigger>
-                    <MenubarContent>
-                        <MenubarItem>New Tab <MenubarShortcut>⌘T</MenubarShortcut></MenubarItem>
-                        <MenubarItem>New Window</MenubarItem>
-                        <MenubarSeparator />
-                        <MenubarItem>Share</MenubarItem>
-                        <MenubarSeparator />
-                        <MenubarItem>Print</MenubarItem>
-                    </MenubarContent>
-                </MenubarMenu>
-            </Menubar>
+                <Menubar className="border-0 shadow-none">
+                    <MenubarMenu>
+                        <MenubarTrigger>View</MenubarTrigger>
+                        <MenubarContent>
+                            <MenubarItem>New Tab <MenubarShortcut>⌘T</MenubarShortcut></MenubarItem>
+                            <MenubarItem>New Window</MenubarItem>
+                            <MenubarSeparator />
+                            <MenubarItem>Share</MenubarItem>
+                            <MenubarSeparator />
+                            <MenubarItem>Print</MenubarItem>
+                        </MenubarContent>
+                    </MenubarMenu>
+                </Menubar>
 
-            <Menubar className="border-0 shadow-none">
-                <MenubarMenu>
-                    <MenubarTrigger>Account</MenubarTrigger>
-                    <MenubarContent>
-                        <MenubarItem>New Tab <MenubarShortcut>⌘T</MenubarShortcut></MenubarItem>
-                        <MenubarItem>New Window</MenubarItem>
-                        <MenubarSeparator />
-                        <MenubarItem>Share</MenubarItem>
-                        <MenubarSeparator />
-                        <MenubarItem>Print</MenubarItem>
-                        
-                    </MenubarContent>
-                </MenubarMenu>
-            </Menubar>
+                <Menubar className="border-0 shadow-none">
+                    <MenubarMenu>
+                        <MenubarTrigger>Account</MenubarTrigger>
+                        <MenubarContent>
+                            <MenubarItem>New Tab <MenubarShortcut>⌘T</MenubarShortcut></MenubarItem>
+                            <MenubarItem>New Window</MenubarItem>
+                            <MenubarSeparator />
+                            <MenubarItem>Share</MenubarItem>
+                            <MenubarSeparator />
+                            <MenubarItem>Print</MenubarItem>
+                            
+                        </MenubarContent>
+                    </MenubarMenu>
+                </Menubar>
+            </div>
+
+            {/* Right-aligned ModeToggle */}
+            <div className="ml-auto">
+                <ModeToggle />
+            </div>
+
         </div>
-
-        {/* Right-aligned ModeToggle */}
-        <div className="ml-auto">
-            <ModeToggle />
-        </div>
-
-      </div>
+      </React.Fragment>
     );
 };
 
